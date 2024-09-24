7191a00e5939aa90ccffa14f61d1dc4a7cea9027
Narendra Ram <narenramm@gmail.com>
2024-09-24 23:49:25 +0530

    simple functions first

--- background.js
+++ background.js
@@ -1,20 +1,8 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "captureImage") {
-    console.log("Captured Image Data:", message.data);
-    // Handle the captured image data (e.g., send to a server or process it)
-  } else if (message.action === "readText") {
-    console.log("Read Text Data:", message.data);
-    // Handle the read text data (e.g., send to a server or process it)
-    chrome.storage.local.set({ signalsData: message.data });
-  }
-});
-
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  if (changes.signalsData) {
-    chrome.action.setPopup({ popup: "popup.html" });
-    chrome.runtime.sendMessage({
-      action: "updateSignals",
-      data: changes.signalsData.newValue,
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "captureTab") {
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+      sendResponse({ imageDataUrl: dataUrl });
     });
+    return true; // Indicates that we will send a response asynchronously
   }
 });
